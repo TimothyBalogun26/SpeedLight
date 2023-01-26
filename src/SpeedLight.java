766d9eaaa21b5e9ca3bba4aa6f2e96eb019f3b86
Timyn <tbalogun@ggc.edu>
2023-01-26 11:40:00 -0500

    commit a line

---
+++ src/SpeedLight.java
@@ -0,0 +1,12 @@
+/**
+ * @class: Speed Light
+ * @author: Timothy Balogun
+ * @course: ITEC 2140-40, Spring 2023
+ */
+
+public class SpeedLight {
+    public static void main(String[] args){
+
+//
+    }
+}
